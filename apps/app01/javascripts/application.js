4a89b0a1d5f5283273dcafcce3c2176a5819dd9a
Nilesh <thakur.nilesh@hotmail.com>
2009-07-25 21:19:53 +0530

    Test1

--- apps/app01/javascripts/application.js
+++ apps/app01/javascripts/application.js
@@ -15,7 +15,35 @@ function $get(key) {
 //***************************************************************************
 // Plugin Handlers
 //***************************************************************************
+ //Trying to Get Product Detail
  
+ var InsertProduct = function() {
+ 
+ var row = "<tr width='100%'>";
+row += "<td>Product: <select><option></option><option>Singulair</option><option>Hyzaar</option></td>";
+row += "<td>Priority: <input type='text' size='1'></input></td>";
+row += "<td>Indication: <select><option></option><option>Allergy</option><option>Asthma</option></td>";
+row += "<td>Issues: <select><option></option><option>Side effects</option><option>Efficacy</option></td>";
+row += "<td><input type='button' name='delete' value='delete' onclick='jQuery(this).parent().parent().remove()'></input></td>";
+row += "<td><input type='button' name='save' value='save' onclick='odlib.saveProdDetail();'></input></td>";
+row += "</tr>";
+ 
+var html = "<div>";
+html += "<table id='mrk_details'>";
+html += row;
+html += "</table>";
+html += "</div>";
+ 
+var e = jQuery("[class='buttonChildTitleBarTD']").filter("[id^='CallsProdDetail']").get(0);
+e.onclick = function() {};
+jQuery("[class='buttonChildTitleBarTD']").filter("[id^='CallsProdDetail']").click(function() {
+if ( jQuery("#mrk_details").size() === 0 ) {
+jQuery("#CallsProdDetailChildListDiv").next().replaceWith(html);
+} else {
+jQuery("#mrk_details").append(row);
+}
+});
+};
 //Trying to Populate the Subject Field
  var copyPreviousSubjectHandler = function() {
         var ownerId = $get('ContactCallInsert.Owner Id').val();
@@ -184,7 +212,8 @@ var copyContactPreviousObjectiveHandler = function() {
     
 };
  
-var augmentCallDetailsEntry = function() {
+var augmentCallDetailsEntry = function() 
+{
 var row = "<tr width='100%'>";
 row += "<td>Product: <select><option></option><option>Singulair</option><option>Hyzaar</option></td>";
 row += "<td>Priority: <input type='text' size='1'></input></td>";
@@ -270,7 +299,13 @@ var pluginsDefinitions = [
         invokeOnPattern: /ContactCallInsert/ig,
         handler: copyPreviousSubjectHandler,
         requiresLogin: true
-    }
+    },
+	{
+	name: 'Insert Product',
+        invokeOnPattern: /ContactCallInsert/ig,
+        handler: InsertProduct,
+        requiresLogin: true
+	}
 ];
  
 //***************************************************************************
@@ -288,77 +323,3 @@ applyPlugins();
     
 });
  
-/*
-var fields = {
-ActivityId: '',
-PrimaryContactId: " ='" + contactPerId + "' ",
-PrimaryContactLastName: '',
-PrimaryContactFirstName: '',
-Owner: '',
-AccountId: '',
-CallType: '',
-PrimaryContact: '',
-CreatedBy: '',
-Location: '',
-Objective: '',
-OwnerId: " ='" + ownerId + "' ",
-Status: '',
-Type: '',
-ActivitySubType: '',
-CreatedDate: '',
-ModifiedDate: '',
-Date: '',
-StartTime: '',
-EndTime: ''
-};
-if (pageName === 'ContactCallDetail') {
-var valueLabel = $( $("td:contains('Objective')")[1] ).next();
-// TODO: implement objective exists logic
-var currentCallObjectiveExists = $.trim( valueLabel.text() ) !== '';
-if (!currentCallObjectiveExists) {
-// autopopulate Objective with previous call objective
-valueLabel.mouseover();
-valueLabel.click();
-var inlineEditor = jQuery('.iled');
-inlineEditor.val((new Date()).toString() + ': last objective' );
-var okButton = inlineEditor.parent().next().children().get(0);
-okButton.click();
-valueLabel.mouseout();
-}
-}
-odlib.activityQuery(fields, function(data) {
-alert( JSON.stringify(data) );
-console.dir(data);
-});
-var userFields = {AccountName:''};
-var entities = [
-{
-name: 'Account',
-fields: {AccountName: ''}
-},
-{
-name: 'Contact',
-fields: {ContactFullName: '', ContactId: ''}
-},
-{
-name: 'User',
-fields: {UserLoginId: '', UserId: ''}
-}
-];
-jQuery.each(entities, function(index, entity) {
-odlib.entityQuery(entity.name, entity.fields, function(data) {
-console.log(entity.name + ' count = ' + data.length);
-});
-});
-odlib.entityQuery('Contact', {ContactFullName: '', ContactId: ''}, function(data) {
-console.dir(data);
-});
-odlib.user_login(userName, password, function() {
-var userFields = ['FirstName', 'LastName'];
-console.log('begin user_login');
-odlib.query_user(userFields, function(data) {
-console.dir(data);
-});
-console.log('end user_login');
-});
-*/
